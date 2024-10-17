9f6759bc2465ed0ef516637dddb64f1166ee5a60
MairaPena <maira.balanta@uao.edu.co>
2024-10-16 21:06:48 -0500

    Se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LocationTracker from './LocationTracker'; // Asegúrate de que el archivo LocationTracker.jsx exista
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPS Dog Tracker</h1>
+      <LocationTracker />
+    </div>
+  );
+};
+
+export default App;
